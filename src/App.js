5ff736fe13c097d5d5291929f926b3735bbb456d
divyarkiet <37652182+divyarkiet@users.noreply.github.com>
2018-12-25 12:27:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Listing from './components/listing.js';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.title = 'User Management System';
+  }
+  render() {
+    const user = {fname: 'Divya', lname: 'Rajput'};
+    function displayName(element){
+      if(element){
+        return <span>Welcome {element.fname+ ' ' +element.lname}</span>;
+      }else{
+        return <span>Welcome Guest</span>;
+      }      
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="left-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>{this.title}</h1>
+          </div>
+          <div className="right-header">{displayName(user)}</div>
+        </header>
+        <section className="app-section">
+          <Listing />                          
+        </section>
+        <footer>
+          
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
